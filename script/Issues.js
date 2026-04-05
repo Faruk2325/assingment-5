58ee3afa1f2bb554344b6f8ce7d2d796f32e11fd
Faruk2325 <of400152@gmail.com>
2026-04-05 06:21:06 -0400

    updated

--- script/Issues.js
+++ script/Issues.js
@@ -209,38 +209,4 @@ buttons.forEach(button =>{
 
 searchInput.addEventListener("input", () =>{
 
-    const query =
-    searchInput.value.toLowerCase();
-
-    const filtered =
-    allIssues.filter(issue =>
-
-        issue.title
-        .toLowerCase()
-        .includes(query)
-
-        ||
-
-        issue.description
-        .toLowerCase()
-        .includes(query)
-
-    );
-
-    displayIssues(filtered);
-
-});
-
-window.addEventListener("load", () =>{
-
-    loader.classList.remove("hidden");
-
-    setTimeout(() =>{
-
-        loader.classList.add("hidden");
-
-    },500);
-
-});
-
-loadIssue();
+   
